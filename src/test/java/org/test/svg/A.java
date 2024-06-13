6bcd3a8357e3de12323085285d066038fc3f9423
Epciban <epciban1995@gmail.com>
2024-06-13 20:18:37 +0530

    Epciban Code

--- src/test/java/org/test/svg/A.java
+++ src/test/java/org/test/svg/A.java
@@ -1,21 +1,20 @@
 package org.test.svg;
 
-public class A { 
-	
+public class A {
+
 	private static void m1() {
 		System.out.println(B.id);
-		
+
 	}
-	
 
 	public static void main(String[] args) {
-		
+
 		m1();
-		
-		
+
 	}
-		
-		
-	
+
+	private void method0() {
+		System.out.println("method0");
+	}
 
 }
